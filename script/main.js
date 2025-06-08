3a776795bddc277f36f3c576d3027aad40c645e0
aida ashrafpour <97738461+aidaash0@users.noreply.github.com>
2025-06-08 20:22:13 +0330

    Update main.js

--- script/main.js
+++ script/main.js
@@ -25,9 +25,6 @@ const animationTimeline = () => {
     const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
     const hbd = document.getElementsByClassName("wish-hbd")[0];
 
-    textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
-        .split("")
-        .join("</span><span>")}</span>`;
 
     hbd.innerHTML = `<span>${hbd.innerHTML
         .split("")
